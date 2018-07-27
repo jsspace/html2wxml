4784d977819f73fba790fd7dee6755787cc23ae9
minyi <minyi@mobvoi.com>
2018-07-27 10:20:21 +0800

    format code style

--- html2wxml/html2json.js
+++ html2wxml/html2json.js
@@ -1,43 +1,41 @@
 /**
  * html2Json 改造来自: https://github.com/Jxck/html2json
- * 
- * 
+ *
+ *
  * author: Di (微信小程序开发工程师)
  * organization: WeAppDev(微信小程序开发论坛)(http://weappdev.com)
  *               垂直微信小程序开发交流社区
- * 
+ *
  * github地址: https://github.com/icindy/wxParse
- * 
+ *
  * for: 微信小程序富文本解析
  * detail : http://weappdev.com/t/wxparse-alpha0-1-html-markdown/184
  */
 
-var __placeImgeUrlHttps = "https";
-var __emojisReg = '';
-var __emojisBaseSrc = '';
-var __emojis = {};
+var __placeImgeUrlHttps = 'https';
 var wxDiscode = require('./wxDiscode.js');
 var HTMLParser = require('./htmlparser.js');
 // Empty Elements - HTML 5
-var empty = makeMap("area,base,basefont,br,col,frame,hr,img,input,link,meta,param,embed,command,keygen,source,track,wbr");
+var empty = makeMap('area,base,basefont,br,col,frame,hr,img,input,link,meta,param,embed,command,keygen,source,track,wbr');
 // Block Elements - HTML 5
-var block = makeMap("br,a,code,address,article,applet,aside,audio,blockquote,button,canvas,center,dd,del,dir,div,dl,dt,fieldset,figcaption,figure,footer,form,frameset,h1,h2,h3,h4,h5,h6,header,hgroup,hr,iframe,ins,isindex,li,map,menu,noframes,noscript,object,ol,output,p,pre,section,script,table,tbody,td,tfoot,th,thead,tr,ul,video");
+var block = makeMap('br,a,code,address,article,applet,aside,audio,blockquote,button,canvas,center,dd,del,dir,div,dl,dt,fieldset,figcaption,figure,footer,form,frameset,h1,h2,h3,h4,h5,h6,header,hgroup,hr,iframe,ins,isindex,li,map,menu,noframes,noscript,object,ol,output,p,pre,section,script,table,tbody,td,tfoot,th,thead,tr,ul,video');
 
 // Inline Elements - HTML 5
-var inline = makeMap("abbr,acronym,applet,b,basefont,bdo,big,button,cite,del,dfn,em,font,i,iframe,img,input,ins,kbd,label,map,object,q,s,samp,script,select,small,span,strike,strong,sub,sup,textarea,tt,u,var");
+var inline = makeMap('abbr,acronym,applet,b,basefont,bdo,big,button,cite,del,dfn,em,font,i,iframe,img,input,ins,kbd,label,map,object,q,s,samp,script,select,small,span,strike,strong,sub,sup,textarea,tt,u,var');
 
 // Elements that you can, intentionally, leave open
 // (and which close themselves)
-var closeSelf = makeMap("colgroup,dd,dt,li,options,p,td,tfoot,th,thead,tr");
+var closeSelf = makeMap('colgroup,dd,dt,li,options,p,td,tfoot,th,thead,tr');
 
 // Attributes that have their values filled in disabled="disabled"
-var fillAttrs = makeMap("checked,compact,declare,defer,disabled,ismap,multiple,nohref,noresize,noshade,nowrap,readonly,selected");
+var fillAttrs = makeMap('checked,compact,declare,defer,disabled,ismap,multiple,nohref,noresize,noshade,nowrap,readonly,selected');
 
 // Special Elements (can contain anything)
-var special = makeMap("wxxxcode-style,script,style,view,scroll-view,block");
+var special = makeMap('wxxxcode-style,script,style,view,scroll-view,block');
+
 function makeMap(str) {
     var obj = {},
-        items = str.split(",");
+        items = str.split(',');
     for (var i = 0; i < items.length; i++) {
         obj[items[i]] = true;
     }
@@ -56,7 +54,7 @@ function removeDOCTYPE(html) {
 }
 
 function trimHtml(html) {
-  return html
+    return html
         .replace(/<!--.*?-->/ig, '')
         .replace(/\/\*.*?\*\//ig, '')
         .replace(/[ ]+</ig, '<')
@@ -73,13 +71,13 @@ function html2json(html, bindName) {
     var results = {
         node: bindName,
         nodes: [],
-        images:[],
-        imageUrls:[]
+        images: [],
+        imageUrls: []
     };
     var index = 0;
     HTMLParser(html, {
         start: function (tag, attrs, unary) {
-            //debug(tag, attrs, unary);
+            // debug(tag, attrs, unary);
             // node for this element
             var node = {
                 node: 'element',
@@ -98,31 +96,31 @@ function html2json(html, bindName) {
             }
 
             if (block[tag]) {
-                node.tagType = "block";
+                node.tagType = 'block';
             } else if (inline[tag]) {
-                node.tagType = "inline";
+                node.tagType = 'inline';
             } else if (closeSelf[tag]) {
-                node.tagType = "closeSelf";
+                node.tagType = 'closeSelf';
             }
 
             if (attrs.length !== 0) {
                 node.attr = attrs.reduce(function (pre, attr) {
                     var name = attr.name;
                     var value = attr.value;
-                    if (name == 'class') {
+                    if (name === 'class') {
                         //  value = value.join("")
                         node.classStr = value;
                     }
                     // has multi attibutes
                     // make it array of attribute
-                    if (name == 'style') {
+                    if (name === 'style') {
                         //  value = value.join("")
                         node.styleStr = value;
                     }
                     if (value.match(/ /)) {
                         value = value.split(' ');
                     }
-                    
+
 
                     // if attr already exists
                     // merge it
@@ -143,11 +141,11 @@ function html2json(html, bindName) {
                 }, {});
             }
 
-            //对img添加额外数据
+            // 对img添加额外数据
             if (node.tag === 'img') {
                 node.imgIndex = results.images.length;
                 var imgUrl = node.attr.src;
-                if (imgUrl[0] == '') {
+                if (imgUrl[0] === '') {
                     imgUrl.splice(0, 1);
                 }
                 imgUrl = wxDiscode.urlToHttpUrl(imgUrl, __placeImgeUrlHttps);
@@ -156,7 +154,7 @@ function html2json(html, bindName) {
                 results.images.push(node);
                 results.imageUrls.push(imgUrl);
             }
-            
+
             // 处理font标签样式属性
             if (node.tag === 'font') {
                 var fontSize = ['x-small', 'small', 'medium', 'large', 'x-large', 'xx-large', '-webkit-xxx-large'];
@@ -169,7 +167,7 @@ function html2json(html, bindName) {
                 if (!node.styleStr) node.styleStr = '';
                 for (var key in styleAttrs) {
                     if (node.attr[key]) {
-                        var value = key === 'size' ? fontSize[node.attr[key]-1] : node.attr[key];
+                        var value = key === 'size' ? fontSize[node.attr[key] - 1] : node.attr[key];
                         node.attr.style.push(styleAttrs[key]);
                         node.attr.style.push(value);
                         node.styleStr += styleAttrs[key] + ': ' + value + ';';
@@ -178,10 +176,10 @@ function html2json(html, bindName) {
             }
 
             //临时记录source资源
-            if(node.tag === 'source'){
+            if (node.tag === 'source') {
                 results.source = node.attr.src;
             }
-            
+
             if (unary) {
                 // if this tag doesn't have end tag
                 // like <img src="hoge.png"/>
@@ -202,11 +200,11 @@ function html2json(html, bindName) {
             if (node.tag !== tag) console.error('invalid state: mismatch end tag');
 
             //当有缓存source资源时于于video补上src资源
-            if(node.tag === 'video' && results.source){
+            if (node.tag === 'video' && results.source) {
                 node.attr.src = results.source;
                 delete results.source;
             }
-            
+
             if (bufArray.length === 0) {
                 results.nodes.push(node);
             } else {
@@ -222,82 +220,27 @@ function html2json(html, bindName) {
             text = text.replace(/&lt;/g, '<');
             var node = {
                 node: 'text',
-                text: text,
-                textArray:transEmojiStr(text)
+                text: text
             };
-            
+
             if (bufArray.length === 0) {
-                node.index = index.toString()
-                index += 1
+                node.index = index.toString();
+                index += 1;
                 results.nodes.push(node);
             } else {
                 var parent = bufArray[0];
                 if (parent.nodes === undefined) {
                     parent.nodes = [];
                 }
-                node.index = parent.index + '.' + parent.nodes.length
+                node.index = parent.index + '.' + parent.nodes.length;
                 parent.nodes.push(node);
             }
-        },
-        comment: function (text) {
-            //debug(text);
-            // var node = {
-            //     node: 'comment',
-            //     text: text,
-            // };
-            // var parent = bufArray[0];
-            // if (parent.nodes === undefined) {
-            //     parent.nodes = [];
-            // }
-            // parent.nodes.push(node);
-        },
+        }
     });
     return results;
-};
-
-function transEmojiStr(str){
-  // var eReg = new RegExp("["+__reg+' '+"]");
-//   str = str.replace(/\[([^\[\]]+)\]/g,':$1:')
-  
-  var emojiObjs = [];
-  //如果正则表达式为空
-  if(__emojisReg.length == 0 || !__emojis){
-      var emojiObj = {}
-      emojiObj.node = "text";
-      emojiObj.text = str;
-      array = [emojiObj];
-      return array;
-  }
-  //这个地方需要调整
-  str = str.replace(/\[([^\[\]]+)\]/g,':$1:')
-  var eReg = new RegExp("[:]");
-  var array = str.split(eReg);
-  for(var i = 0; i < array.length; i++){
-    var ele = array[i];
-    var emojiObj = {};
-    if(__emojis[ele]){
-      emojiObj.node = "element";
-      emojiObj.tag = "emoji";
-      emojiObj.text = __emojis[ele];
-      emojiObj.baseSrc= __emojisBaseSrc;
-    }else{
-      emojiObj.node = "text";
-      emojiObj.text = ele;
-    }
-    emojiObjs.push(emojiObj);
-  }
-  
-  return emojiObjs;
-}
-
-function emojisInit(reg='',baseSrc="/wxParse/emojis/",emojis){
-    __emojisReg = reg;
-    __emojisBaseSrc=baseSrc;
-    __emojis=emojis;
 }
 
 module.exports = {
-    html2json: html2json,
-    emojisInit:emojisInit
+    html2json: html2json
 };
 
---
+++ test/htmlparser.test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by minyi on 2018/7/26.
+ */
+var HTMLParser = require('../html2wxml/htmlparser');
+
+var html = '<div>aaa</div>';
+
+console.log(HTMLParser(html, {
+
+}));
